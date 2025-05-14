ea0a24ad2bf0d04b2d66bf27240e049a3306eee5
abhi9720 <abhishektiwari9720@gmail.com>
2025-05-15 00:44:57 +0530

    port added

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 	"pastebin-backend/internal/database"
 	"pastebin-backend/internal/router"
 	"pastebin-backend/internal/utils/cron"
@@ -10,22 +11,26 @@ import (
 )
 
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
+	database.Connect()
+	database.Migrate()
 
-    database.Connect()
-    database.Migrate()
+	r := router.SetupRouter()
+	cron.StartUploadCleanupJob()
 
-    r := router.SetupRouter()
-    cron.StartUploadCleanupJob()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default to 8080 if not provided (for local development)
+	}
 
-    // Start server
-    if err := r.Run(":8080"); err != nil {
-        log.Fatalf("Failed to run server: %v", err)
-    }
+	// Start server
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
 }
 
 // expernal postgresql://pastebindb_pncl_user:Psbw8xYEQJfYrmt6X4nrw7PsKF6iBIJI@dpg-d0hgtjh5pdvs73egf4o0-a.oregon-postgres.render.com/pastebindb_pncl
